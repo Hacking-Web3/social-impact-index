1d35fe7bfdd4b3584a960f1bf4fdd4630f1b768b
leon <leon.ducasse@epitech.eu>
2022-04-03 02:14:12 +0200

    [ADD] donate methods + test

--- packages/hardhat-ts/test/myTest.js
+++ packages/hardhat-ts/test/myTest.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+const { ethers, network } = require("hardhat");
 const { use, expect } = require("chai");
 const { solidity } = require("ethereum-waffle");
 
@@ -12,7 +12,7 @@ const { tryCatch } = require("ramda");
 
 use(solidity);
 
-describe("My Dapp", function () {
+describe("SIO Registry", function () {
   //let myContract;
 
   // quick fix to let gas reporter fetch data from gas station & coinmarketcap
@@ -20,7 +20,7 @@ describe("My Dapp", function () {
     setTimeout(done, 2000);
   });
 
-  describe("Collect", function () {
+  describe("Collect, Add SIO/Index", function () {
     it("Should deploy Collect", async function () {
       const CollectContract = await ethers.getContractFactory("Collect");
 
@@ -156,7 +156,7 @@ describe("My Dapp", function () {
       const newData = [["newCeramicStream", newAddress, false]];
       const newIndexes = [1];
 
-      await expectRevert(collectContract.setSIOs(newIndexes, newData), "Not the owner or unavailable ID");
+      await expect(collectContract.setSIOs(newIndexes, newData)).to.be.revertedWith("Not the owner or unavailable ID");
     });
 
     it("Creation of a new index", async function () {
@@ -256,8 +256,117 @@ describe("My Dapp", function () {
       const indexIDs = [1, 4];
       const shares = [5000, 3000];
 
-      await expectRevert(impCollectContract.createIndex(indexIDs, shares), "The repartition of the shares is different than 100%");
+      await expect(impCollectContract.createIndex(indexIDs, shares)).to.be.revertedWith("The repartition of the shares is different than 100%");
+    });
+  });
+
+  describe("Collect, donate", function () {
+    it("Donate to SIO", async function () {
+      await network.provider.request({
+        method: 'hardhat_reset',
+        params: [
+          {
+            forking: {
+              jsonRpcUrl: 'https://eth-mainnet.alchemyapi.io/v2/LjjqK5PekBuJj8FxfyX2ZZLcU1HYZWvI',
+              blockNumber: 12964900,
+            },
+          },
+        ],
+      });
+
+      const CollectContract = await ethers.getContractFactory("Collect");
+      const collectContract = await CollectContract.deploy();
+
+      await network.provider.request({
+        method: 'hardhat_impersonateAccount',
+        params: ['0xf60c2Ea62EDBfE808163751DD0d8693DCb30019c'],
+      });
+
+      const signer = await ethers.getSigner('0xf60c2Ea62EDBfE808163751DD0d8693DCb30019c');
+      const impCollectContract = collectContract.connect(signer);
+
+      const addressONE = signer.address;
+      const addressTWO = "0x130e7436fa0fb04ebd2568faf2780fcf11774583"
+      const addressTHREE = "0xe0af683a87495380a80f91bde8dc4fbed1421357"
+
+      const dataOne = ["ceramicStreamOne", addressONE, true];
+      const dataTwo = ["ceramicStreamTwo", addressTWO, false];
+      const dataThree = ["ceramicStreamThree", addressTHREE, true];
+
+      const IDs = [1, 4, 2]
+      const datas = [dataOne, dataTwo, dataThree];
+
+
+      const tx = await collectContract.setSIOs(IDs, datas);
+      await tx.wait();
+
+      const amountAddrTwoBefore = await collectContract.provider.getBalance(addressTWO);
+
+      const tx2 = await impCollectContract.donateToSIO(4, { value: Math.pow(10, 18).toString() });
+      tx2.wait();
+
+      const amountAddrTwoAfter = await collectContract.provider.getBalance(addressTWO);
+      expect(amountAddrTwoAfter).to.above(amountAddrTwoBefore);
     });
 
+    it("Donate to Index", async function () {
+      await network.provider.request({
+        method: 'hardhat_reset',
+        params: [
+          {
+            forking: {
+              jsonRpcUrl: 'https://eth-mainnet.alchemyapi.io/v2/LjjqK5PekBuJj8FxfyX2ZZLcU1HYZWvI',
+              blockNumber: 12964900,
+            },
+          },
+        ],
+      });
+
+      const CollectContract = await ethers.getContractFactory("Collect");
+      const collectContract = await CollectContract.deploy();
+
+      await network.provider.request({
+        method: 'hardhat_impersonateAccount',
+        params: ['0xf60c2Ea62EDBfE808163751DD0d8693DCb30019c'],
+      });
+
+      const signer = await ethers.getSigner('0xf60c2Ea62EDBfE808163751DD0d8693DCb30019c');
+      const impCollectContract = collectContract.connect(signer);
+
+      const addressONE = signer.address;
+      const addressTWO = "0x130e7436fa0fb04ebd2568faf2780fcf11774583"
+      const addressTHREE = "0xe0af683a87495380a80f91bde8dc4fbed1421357"
+      const addressFOUR = "0x3746376FAB142D2259ADC228Fc406F678B29fBC3"
+
+      const dataOne = ["ceramicStreamOne", addressONE, true];
+      const dataTwo = ["ceramicStreamTwo", addressTWO, false];
+      const dataThree = ["ceramicStreamThree", addressTHREE, true];
+      const dataFOUR = ["ceramicStreamThree", addressFOUR, true];
+
+      const IDs = [1, 4, 2, 15]
+      const datas = [dataOne, dataTwo, dataThree, dataFOUR];
+
+
+      const tx = await impCollectContract.setSIOs(IDs, datas);
+      await tx.wait();
+
+      const indexIDs = [2, 15];
+      const shares = [5000, 5000];
+
+      const amountAddrTwoBefore = await impCollectContract.provider.getBalance(addressTHREE);
+      const amountAddrFOURBefore = await impCollectContract.provider.getBalance(addressFOUR)
+
+      const tx2 = await impCollectContract.createIndex(indexIDs, shares);
+      tx2.wait();
+
+      const tx3 = await impCollectContract.donateToIndex(addressONE, 0, { value: (5 * Math.pow(10, 18)).toString() });
+      tx3.wait();
+
+      const amountAddrTwoAfter = await impCollectContract.provider.getBalance(addressTHREE);
+      const amountAddrFOURAfter = await impCollectContract.provider.getBalance(addressFOUR)
+
+      expect(amountAddrTwoAfter).to.above(amountAddrTwoBefore);
+      expect(amountAddrFOURAfter).to.above(amountAddrFOURBefore);
+    });
   });
 });
