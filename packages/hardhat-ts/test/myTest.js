b56e37695647d8410330d09e3aea3f604207fd61
leon <leon.ducasse@epitech.eu>
2022-05-18 18:43:00 +0200

    [ADD] test to update an index KO bc of invalid index ID

--- packages/hardhat-ts/test/myTest.js
+++ packages/hardhat-ts/test/myTest.js
@@ -519,5 +519,59 @@ describe("Profile Registry", function () {
       expect(userIndexesAfterTx[0].shares[0]).to.equal(2500);
       expect(userIndexesAfterTx[0].shares[1]).to.equal(7500);
     });
+
+    it("Update an index KO Invalid Index ID", async function () {
+      await network.provider.request({
+        method: 'hardhat_reset',
+        params: [
+          {
+            forking: {
+              jsonRpcUrl: 'https://eth-mainnet.alchemyapi.io/v2/LjjqK5PekBuJj8FxfyX2ZZLcU1HYZWvI',
+              blockNumber: 12964900,
+            },
+          },
+        ],
+      });
+
+      const CollectContract = await ethers.getContractFactory("Collect");
+      const collectContract = await CollectContract.deploy();
+
+      await network.provider.request({
+        method: 'hardhat_impersonateAccount',
+        params: ['0xf60c2Ea62EDBfE808163751DD0d8693DCb30019c'],
+      });
+
+      const signer = await ethers.getSigner('0xf60c2Ea62EDBfE808163751DD0d8693DCb30019c');
+      const impCollectContract = collectContract.connect(signer);
+
+      const addressA = signer.address;
+      const addressB = "0x130e7436fa0fb04ebd2568faf2780fcf11774583"
+
+      const addressC = "0xe0af683a87495380a80f91bde8dc4fbed1421357"
+      const addressD = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+
+      const dataA = ["ceramicStreamA", addressA, true];
+      const dataB = ["ceramicStreamB", addressB, false];
+
+      const dataC = ["ceramicStreamC", addressC, true];
+      const dataD = ["ceramicStreamD", addressD, false];
+
+      const IDs = [1, 2, 69, 420];
+      const datas= [dataA, dataB, dataC, dataD];
+
+      const txAddProfiles = await collectContract.addProfiles(IDs, datas);
+      await txAddProfiles.wait();
+
+      const profileIDs = [1, 2];
+      const shares = [5000, 5000];
+
+      const newProfileIDs = [69, 420];
+      const newShares = [2500, 7500];
+
+      const txCreateIndex = await impCollectContract.createIndex(profileIDs, shares);
+      txCreateIndex.wait();
+
+      await expect(impCollectContract.updateIndex(42, newProfileIDs, newShares)).to.be.revertedWith("Invalid index ID");
+    });
   });
 });
